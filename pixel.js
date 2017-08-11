4ddd6466147ce0ecc3803ddb4529b8cb2fbf5a60
Benjamin Wilson <wils0859@gmail.com>
2017-08-11 17:14:25 -0600

    Added color palette i number

--- pixel.js
+++ pixel.js
@@ -16,18 +16,22 @@ function createGrid() {
 
 function createColors() {
   let place = document.querySelector('.colors')
-  const colors = ['black', 'white', 'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'cyan']
+  const colors = ['black', 'white', 'red', '#FF5310', 'yellow', 'green', 'blue', 'indigo', 'violet', 'cyan']
   for (let c = 0; c < colors.length; c++) {
     let currentColor = colors[c]
-    console.log(currentColor + " is the current color")
     let rowColor = document.createElement('div')
     rowColor.classList.add('colorBox')
     rowColor.style.backgroundColor = currentColor
+    let pixelID2 = rowColor.id=(`color${[c]}`)
     place.appendChild(rowColor)
 
 
   } let listen = document.querySelector('.container')
     listen.addEventListener('click', function() {
+      console.log(event.target.id)
+    })
+    let listen2 = document.querySelector('.colors')
+    listen2.addEventListener('click', function() {
       console.log(event.target.id)
     })
 }
